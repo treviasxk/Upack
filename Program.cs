d964fc7706f2fbd771741851c73876a52a6ef514
Trevias Xk <trevias@live.com>
2022-07-14 09:05:38 -0300

    • Criado class UpackManifest para organizar dados. • Adicionado chamadas para o evento OnErrorUpdate. • Outras melhorias.

--- Program.cs
+++ Program.cs
@@ -5,18 +5,27 @@
 Upack.OnUpackStatus = OnUpackStatus;
 Upack.OnUpdateCompleted = OnCompleted;
 Upack.OnErrorUpdate = OnError;
-await Upack.UpdateFilesAsync(Environment.CurrentDirectory, "http://192.168.0.101/data/builds/Manifest.txt");
-//upack.CreateManifest("AssetBundles", "http://192.168.0.101/data/builds/android", "0.0.1.0 (alpha)");
+await Upack.UpdateFilesAsync(Environment.CurrentDirectory + "/AssetBundles", "http://192.168.0.101/data/builds/android.upack");
 Console.ReadKey();
-
+/*
+UpackManifest manifest = new UpackManifest();
+manifest.Name = "Alpha Xk";
+manifest.Version = "0.0.1.0 (alpha)";
+manifest.BuildVersion = "0.0.1.0 (alpha)";
+manifest.Status = UpackStatus.Online;
+manifest.Message = "We are experiencing some instability, please try again later.";
+manifest.WebSite = "http://192.168.0.101";
+manifest.UrlPath = "http://192.168.0.101/data/builds/android";
+Upack.CreateManifest("AssetBundles", manifest);
+Console.ReadKey();*/
 void OnError(){
     Console.WriteLine("Error!");
 }
 
-void OnCompleted(string _version){
+void OnCompleted(UpackManifest _manifest){
     Console.WriteLine("Completed!");
 }
 
 void OnUpackStatus(DownloadInfo _info){
-    Console.WriteLine(_info.Status + " " + _info.filename + " " + _info.progress + "%" + " " + (_info.bytesReceived != null ? _info.bytesReceived + "/" + _info.totalBytes : ""));
+    Console.WriteLine(_info.Status + " " + _info.filename + " "+ _info.speedDownload+" " + _info.progress + "%" + " " + (_info.bytesReceived != null ? _info.bytesReceived + "/" + _info.totalBytes : ""));
 }
--- upack.cs
+++ upack.cs
@@ -15,66 +15,89 @@ public class DownloadInfo{
     public string bytesReceived {get;set;}
     public int progress {get;set;}
 }
+public enum UpackStatus {Offline, Online, Maintenance};
+public class UpackManifest{
+    public string Name {get;set;}
+    public string Version {get;set;}
+    public string BuildVersion {get;set;}
+    public UpackStatus Status {get;set;}
+    public string Message {get;set;}
+    public string WebSite {get;set;}
+    public string UrlPath {get;set;}
+}
+
 public class Upack{
     public static Action<DownloadInfo> OnUpackStatus;
-    public static Action<string> OnUpdateCompleted;
+    public static Action<UpackManifest> OnUpdateCompleted;
     public static Action OnErrorUpdate;
     static Dictionary<string, string> dwfile = new Dictionary<string, string>();
     static Dictionary<string, string> dwlast = new Dictionary<string, string>();
     static HttpClient webClient = new HttpClient();
-    static int progress;
+    static int progress, time, speed;
     
-    static string version, urlAsset, pathFiles;
-    public static void CreateManifest(string PathFiles, string URLFolder, string Version = ""){
-        var fullpath =  Path.GetFullPath(PathFiles);
-        PathFiles = Path.GetFullPath(PathFiles);
-        PathFiles = PathFiles.Replace(Path.GetDirectoryName(PathFiles) + "\\","");
+    static string pathFiles;
+
+    public static void CreateManifest(string PathFiles, UpackManifest manifest){
+        string fullpath =  Path.GetFullPath(PathFiles) + "\\";
+        PathFiles = PathFiles.Replace(Path.GetDirectoryName(fullpath) + "\\","");
 
         string data = "UPACK" + "\n";
-        data += "version=" + Version + "\n";
-        data += "urlpath=" + URLFolder + "\n";
+        data += "Name=" + manifest.Name + "\n";
+        data += "Version=" + manifest.Version + "\n";
+        data += "BuildVersion=" + manifest.BuildVersion + "\n";
+        data += "Status=" + Convert.ToInt32(manifest.Status) + "\n";
+        data += "Message=" + manifest.Message + "\n";
+        data += "WebSite=" + manifest.WebSite + "\n";
+        data += "UrlPath=" + manifest.UrlPath + "\n";
 
         var files = Directory.GetFiles(fullpath, "*.*", SearchOption.AllDirectories);
-        data += "total=" + files.Count() + "\n";
+        data += "TotalFiles=" + files.Count() + "\n";
         data += "\n";
         foreach (string file in files) {
-            var xx = Path.GetFullPath(file);
-            data += PathFiles + xx.Replace(fullpath,"").Replace("\\","/") + "\n";
+            data += Path.GetFullPath(file).Replace(fullpath,"").Replace("\\","/") + "\n";
         }
         data += "\n";
 
         foreach (string file in files) {
             data += CalculateMD5(file) + "\n";
         }
-
-        File.WriteAllText("Manifest.txt", Convert.ToBase64String(Encoding.ASCII.GetBytes(data)), Encoding.ASCII);
+        File.WriteAllText("Manifest.upack", Convert.ToBase64String(Encoding.ASCII.GetBytes(data)), Encoding.ASCII);
     }
 
+    static UpackManifest MyManifest;
     public static async Task UpdateFilesAsync(string PATH, string URL){
-        if(!PATH.EndsWith("/"))
-            PATH += "/";
-        pathFiles = PATH.Replace("\\","/");
+        pathFiles = Path.GetFullPath(PATH).Replace("\\","/") + "/";
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         dwfile.Clear();
         dwlast.Clear();
         progress = 0;
+        try{
+            string data = await webClient.GetStringAsync(URL);
+            string[] result = Encoding.ASCII.GetString(Convert.FromBase64String(data)).Split(new string[] {"\n"}, StringSplitOptions.None);
+            if(result[0] == "UPACK"){
+                MyManifest = new UpackManifest();
+                MyManifest.Name = result[1].Replace("Name=","");
+                MyManifest.Version = result[2].Replace("Version=","");
+                MyManifest.BuildVersion = result[3].Replace("BuildVersion=","");
+                MyManifest.Status = (UpackStatus)Convert.ToInt32(result[4].Replace("Status=",""));
+                MyManifest.Message = result[5].Replace("Message=","");
+                MyManifest.WebSite = result[6].Replace("WebSite=","");
+                MyManifest.UrlPath = result[7].Replace("UrlPath=","");
 
-        string data = await webClient.GetStringAsync(URL);
-        string[] result = Encoding.ASCII.GetString(Convert.FromBase64String(data)).Split(new string[] {"\n"}, StringSplitOptions.None);
+                if(!MyManifest.UrlPath.EndsWith("/"))
+                    MyManifest.UrlPath += "/";
+                
+                int TotalFiles = Convert.ToInt32(result[8].Replace("TotalFiles=",""));
+                string[] files = new string[TotalFiles];
+                string[] md5 = new string[TotalFiles];
 
-        if(result[0] == "UPACK"){
-            version = result[1].Replace("version=","");
-            urlAsset = result[2].Replace("urlpath=","");
-            if(!urlAsset.EndsWith("/"))
-                urlAsset += "/";
-            int totalfiles = Convert.ToInt32(result[3].Replace("total=",""));
-            string[] files = new string[totalfiles];
-            string[] md5 = new string[totalfiles];
-
-            files =  result.Skip(5).ToArray().Take(totalfiles).ToArray();
-            md5 =  result.Skip(6 + totalfiles).ToArray();
-            await CheckFilesAsync(files,md5);
-        }else{
+                files =  result.Skip(10).ToArray().Take(TotalFiles).ToArray();
+                md5 =  result.Skip(11 + TotalFiles).ToArray();
+                await CheckFilesAsync(files,md5);
+            }else{
+                OnErrorUpdate?.Invoke();
+            }
+        }catch{
             OnErrorUpdate?.Invoke();
         }
     }
@@ -97,51 +120,56 @@ public class Upack{
     static async Task DownloadFileAsync(){
         bool pass = true;
         for(int i = 0; i < dwfile.Count; i++){
-            string _url = urlAsset + dwfile.ElementAt(i).Key;
+            string _url = MyManifest.UrlPath + dwfile.ElementAt(i).Key;
             string _location = pathFiles + dwfile.ElementAt(i).Key;
             new FileInfo(_location).Directory.Create();
-            HttpClient client = new HttpClient();
-            var response = await client.GetAsync(_url, HttpCompletionOption.ResponseHeadersRead);
-            
-            var size = response.Content.Headers.ContentLength;
-            response.EnsureSuccessStatusCode();
+            try{
+                var response = await webClient.GetAsync(_url, HttpCompletionOption.ResponseHeadersRead);
+                var size = response.Content.Headers.ContentLength;
+                response.EnsureSuccessStatusCode();
+                var contentStream = await response.Content.ReadAsStreamAsync();
+                var buffer = new byte[(int)size];
+                int totalBytes = 0;
 
-            var contentStream = await response.Content.ReadAsStreamAsync();
-            var buffer = new byte[(int)size];
-            int totalBytes = 0;
+                GC.Collect(); 
+                GC.WaitForPendingFinalizers();
+                if(File.Exists(_location))
+                    if(new FileInfo(_location).Length - 1 >= size)
+                        File.Delete(_location);
 
-            GC.Collect(); 
-            GC.WaitForPendingFinalizers();
-            if(File.Exists(_location))
-                if(new FileInfo(_location).Length - 1 >= size)
-                    File.Delete(_location);
-
-            using(var fileStream = new FileStream(_location, FileMode.Create, FileAccess.ReadWrite, FileShare.None, buffer.Length, true)){
-                do{
-
-                    float x = (100 / (float)dwfile.Count);
-                    progress = (int)((x / (float)size) * totalBytes) + (int)(i * x);
-                    OnUpackStatus?.Invoke(new DownloadInfo {filename = dwfile.ElementAt(i).Key, progress = progress, Status = StatusFile.Downloading, bytesReceived = GetSizeShow(totalBytes), totalBytes =  GetSizeShow((int)size)});
-                    var bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length);
-                    if(bytesRead == 0)
-                        continue;
-                    await fileStream.WriteAsync(buffer, 0, bytesRead);
-                    totalBytes += bytesRead;
+                using(var fileStream = new FileStream(_location, FileMode.Create, FileAccess.ReadWrite, FileShare.None, buffer.Length, true)){
+                    do{
+                        float x = (100 / (float)dwfile.Count);
+                        progress = (int)((x / (float)size) * totalBytes) + (int)((i + 1) * x);
+                        if(time != DateTime.Now.Second){
+                            time = DateTime.Now.Second;
+                            speed = totalBytes - speed;
+                        }
+                        OnUpackStatus?.Invoke(new DownloadInfo {speedDownload = GetSizeShow(speed), filename = dwfile.ElementAt(i).Key, progress = progress, Status = StatusFile.Downloading, bytesReceived = GetSizeShow(totalBytes), totalBytes =  GetSizeShow((int)size)});
+                        var bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+                        if(bytesRead == 0)
+                            continue;
+                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+                        totalBytes += bytesRead;
+                    }
+                    while (totalBytes < size);
                 }
-                while (totalBytes < size);
-            }
-            if(CalculateMD5(_location) == dwfile.ElementAt(i).Value){
-                OnUpackStatus?.Invoke(new DownloadInfo {filename = dwfile.ElementAt(i).Key, progress = progress, Status = StatusFile.Updated, bytesReceived = GetSizeShow(totalBytes), totalBytes =  GetSizeShow((int)size)});
-            }else{
+                if(CalculateMD5(_location) == dwfile.ElementAt(i).Value){
+                    OnUpackStatus?.Invoke(new DownloadInfo {filename = dwfile.ElementAt(i).Key, progress = progress, Status = StatusFile.Updated, bytesReceived = GetSizeShow(totalBytes), totalBytes =  GetSizeShow((int)size)});
+                }else{
+                    pass = false;
+                    OnUpackStatus?.Invoke(new DownloadInfo {filename = dwfile.ElementAt(i).Key, Status = StatusFile.Failed});
+                }
+            }catch{
                 pass = false;
-                OnUpackStatus?.Invoke(new DownloadInfo {filename = dwfile.ElementAt(i).Key, progress = progress, Status = StatusFile.Failed, bytesReceived = GetSizeShow(totalBytes), totalBytes =  GetSizeShow((int)size)});
+                OnUpackStatus?.Invoke(new DownloadInfo {filename = dwfile.ElementAt(i).Key, Status = StatusFile.Failed});
             }
         }
 
         if(!pass)
             OnErrorUpdate?.Invoke();
         else
-            OnUpdateCompleted?.Invoke(version);
+            OnUpdateCompleted?.Invoke(MyManifest);
     }
 
     static MD5 md5 = MD5.Create();
